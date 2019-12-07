6c436198f083403db350f336e2777bda8980ec41
Michel van Heest <michelvanheest@icloud.com>
2019-12-07 14:05:21 +0100

    Fixed smooth scroll on Safari

--- site/templates/default.php
+++ site/templates/default.php
@@ -49,7 +49,7 @@
                         <div class="card-body">
                           <?= $page->introduction_tldr()->kirbytext() ?>
                           <?= $page->introduction_tldr_list()->kirbytext() ?>
-                          <a id="Skip button" class="btn btn-info" href="#pipeline" role="button"><i class="fa fa-forward"></i> Skip uitleg</a>
+                          <a id="skipLeren" class="btn btn-info" href="#pipeline" role="button"><i class="fa fa-forward"></i> Skip uitleg</a>
                         </div>
                       </div>
                     </div>
@@ -120,7 +120,7 @@
                       <div class="card-body">
                         <?= $page->pipelining_tldr()->kirbytext() ?>
                         <?= $page->pipelining_tldr_list()->kirbytext() ?>
-                        <a id="Skip button" class="btn btn-info" href="#opdracht" role="button"><i class="fa fa-forward"></i> Skip uitleg</a>
+                        <a id="skipPipelines" class="btn btn-info" href="#opdracht" role="button"><i class="fa fa-forward"></i> Skip uitleg</a>
                       </div>
                     </div>
                   </div>
@@ -163,7 +163,7 @@
                         <div class="card-body">
                           <?= $page->opdracht_stappen()->kirbytext() ?>
                           <?= $page->opdracht_stappen_list()->kirbytext() ?>
-                          <a id="Skip button" class="btn btn-info" href="#figmastuk" role="button"><i class="fa fa-forward"></i> Naar figma</a>
+                          <a id="skipOpdracht" class="btn btn-info" href="#figmastuk" role="button"><i class="fa fa-forward"></i> Naar figma</a>
                         </div>
                       </div>
                       <div class="card tldr">
@@ -254,9 +254,10 @@
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="assets\js\smooth-scrolling.js"></script>
+        <script type="text/javascript" src="assets\js\smooth-scrolling.js"></script>
     </body>
 </html>
