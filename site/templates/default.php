f2a47da1f67fd49b6c5fa14349e8daa558bc84ae
Michel van Heest <michelvanheest@icloud.com>
2019-12-07 21:03:13 +0100

    carouselahoy🚢

--- site/templates/default.php
+++ site/templates/default.php
@@ -83,6 +83,52 @@
                     </div>
                     <div class="col-12 tekst-small">
                         <?= $page->pipelining_uitleg_title()->kirbytext() ?>
+                        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-controls">
+                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                                <div class="carousel-item card active">
+                                    <div>
+                                        <h3>Een opgemaakt rapport met echte data</h3>
+                                        <div class="carousel-content">
+                                            <?= $page->pipelining_carousel_1()->kirbytext() ?>
+                                            <?= $page->pipelining_carousel_1_img()->kirbytext() ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item card">
+                                    <div>
+                                        <h3>Een opgemaakt rapport met echte data</h3>
+                                        <div class="carousel-content">
+                                            <?= $page->pipelining_carousel_1()->kirbytext() ?>
+                                            <?= $page->pipelining_carousel_1_img()->kirbytext() ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="carousel-item card">
+                                    <div>
+                                        <h3>Een opgemaakt rapport met echte data</h3>
+                                        <div class="carousel-content">
+                                            <?= $page->pipelining_carousel_1()->kirbytext() ?>
+                                            <?= $page->pipelining_carousel_1_img()->kirbytext() ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
